78429abfd754875fd15790362fe81895f36ec66e
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-03-11 15:27:24 +0700

    style: Update contant files

---
+++ lib/views/utils/contants/number_contants.dart
@@ -0,0 +1,31 @@
+class NumberContants {
+  static const double FONT_SIZE_16 = 16.0;
+
+  static const double FONT_SIZE_40 = 40.0;
+
+  static const double FONT_SIZE_28 = 28.0;
+
+  static const double VERTICAL_40 = 40.0;
+
+  static const double HORIZONTAL_28 = 28.0;
+
+  static const double HEIGHT_48 = 48.0;
+
+  static const double ELEVATION_0 = 0;
+
+  static const double BORDER_RADIUS_15 = 15.0;
+
+  static const double HEIGHT_24 = 24.0;
+
+  static const double HORIZONTAL_24 = 24.0;
+
+  static const double HEIGHT_22 = 22.0;
+
+  static const double DOT_HEIGHT_6 = 6.0;
+
+  static const double DOT_WIDTH_6 = 6.0;
+
+  static const int MILISECONDS_500 = 500;
+
+  static const double VERTICAL_14 = 14.0;
+}
---
+++ lib/views/utils/contants/string_contants.dart
@@ -0,0 +1,42 @@
+class StringContants {
+  static const String INITIAL_ROUTE = '/';
+
+  static const String SPLASH_SCREEN_ROUTE = '/splash_screen';
+
+  static const String HOME_SCREEN_ROUTE = '/home_screen';
+
+  static const String NEXT_TITLE = 'Next';
+
+  static const String SIGN_UP_TITLE = 'Sign me up!';
+
+  static const List<Map<String, String>> ONBOARDING_DATA = <Map<String, String>>[
+    <String, String>{
+      'circleImagePath': 'assets/images/circle_background_1.png',
+      'illustrationImagePath': 'assets/images/illustration_1.png',
+      'title': 'Welcome!',
+      'subTitle':
+          'It’s a pleasure to meet you. We are excited that you’re here so let’s get started!',
+    },
+    <String, String>{
+      'circleImagePath': 'assets/images/circle_background_2.png',
+      'illustrationImagePath': 'assets/images/illustration_2.png',
+      'title': 'Irrelevant results again?',
+      'subTitle':
+          'No need to rummage through irrelevant items anymore, we got you covered. PrimeMart sends you relevant items based off of your habits and interests.',
+    },
+    <String, String>{
+      'circleImagePath': 'assets/images/circle_background_3.png',
+      'illustrationImagePath': 'assets/images/illustration_3.png',
+      'title': 'Your interests working with you.',
+      'subTitle':
+          'Tell us what you like. No, really, it helps a bunch when we serve you some great products. You just keep doing your thing.',
+    },
+    <String, String>{
+      'circleImagePath': 'assets/images/circle_background_4.png',
+      'illustrationImagePath': 'assets/images/illustration_4.png',
+      'title': 'And that’s the cherry on top!',
+      'subTitle':
+          'No fees, free shipping and amazing customer service. We’ll get you your package within 2 business days no questions asked!',
+    },
+  ];
+}
